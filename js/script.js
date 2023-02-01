1aa16067f3e8ce0e954bb839a3ecc1beffd9211c
Umang Rathod <87117456+rockypagal@users.noreply.github.com>
2023-02-01 22:00:32 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,58 +1,99 @@
 
 
-let form = document.querySelector('form');
-
+let form = document.querySelector('.sub');
+let table = document.querySelector('table');
 let p;
 let r;
 let n;
 let monthly;
+let total;
+let due;
+let interest;
+let ans = document.querySelector('.ans1');
+let amount = document.querySelector('.money');
+let month = document.querySelector('.time')
+let duee = document.querySelector('.half');
+let funcount = 0;
 
-let amount = document.querySelector('.amount');
-let month = document.querySelector('.interest')
-let duee = document.querySelector('.due');
-let tot = document.querySelector('.total');
-form.addEventListener('submit',(e)=>{
+
+form.addEventListener('click',(e)=>{
+console.log(funcount,'top');
 e.preventDefault();
 p = document.querySelector('.p').value;
 r = document.querySelector('.r').value;
 n = document.querySelector('.n').value;
-console.log(p,n,r);
-let table = document.querySelector('.table');
+
+
 let temp = p;
 let emi = Math.round((p*(r/1200)*((1+(r/1200))**(n*12)))/(((1+(r/1200))**(n*12))-1));
 
-amount.innerHTML = "";
-month.innerHTML = "";
-duee.innerHTML = "";
-tot.innerHTML = "";
 
-for (let i = 0; i < n*12; i++) {
+// if(funcount > 0) {
+//     deletetable();
+//     console.log(funcount,"f");
+// }
+
+for (let i = 1; i <=n*12; i++) {
 
 
-let total = Math.round((r/100)*temp/12);
+total = Math.round((r/100)*temp/12);
 monthly = Math.round(emi-total);
-let due = temp - monthly;
-let interest = emi-monthly;
+due = temp - monthly;
+interest = emi-monthly;
 
-console.log(monthly);
-console.log(interest);
-console.log(due);
+// console.log(monthly);
+// console.log(interest);
+// console.log(due);
+// console.log(due);
+
+
 temp = due;
-console.log(due);
+let tbody = document.querySelector('tbody');
+let count2 = 0;
+let arr = [i,monthly,interest,emi,due];
 
-// let text = document.createTextNode(monthly);
-// td.appendChild(text);
+function doit() {
 
+    
+    let count = 0;
+     let row = document.createElement('tr');
+    for (let i=1; i<=5; i++) {
+    
+    let col = document.createElement('td');
+    let textCell = document.createTextNode(arr[count++]);
+    col.appendChild(textCell);
+    row.appendChild(col);
+    tbody.appendChild(row);
+    table.appendChild(tbody);
+    
+    }
+    }
 
+doit();
 
-amount.innerHTML += "<br/>" +monthly+"<br/>";
-month.innerHTML += "<br/>"+interest+"<br/>";
-duee.innerHTML += "<br/>" +due+"<br/>";
-tot.innerHTML += "<br/>" +emi +"<br/>";
 }
 
+month.innerHTML = n*12;
+amount.innerHTML = p;
+duee.innerHTML = emi;
+
+if(funcount == 0) {
+    funcount++;
+}
+
+console.log(funcount,"last");
 });
 
 
 
- 
+
+
+// function deletetable(){
+//     funcount = 0;
+//     for(let j=0; j<=n*12; j++) {
+//         table.deleteRow(0);
+//     }
+    
+//     console.log(funcount,'outsid');
+// }
+
